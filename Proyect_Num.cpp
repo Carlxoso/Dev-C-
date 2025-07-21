e21e10f4e5b2d1f592830696d7eec44c9ed9914e
Carlxoso_ <133054537+Carlxoso@users.noreply.github.com>
2025-07-21 14:15:10 -0500

    Inicio del proyecto

---
+++ Proyect_Num.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int numero = 7;
+    int numero2 = 20;
+    
+    cout << "El numero es: " << numero2 << endl;
+
+    cout << "El numero es: " << numero << endl;
+
+    return 0;
+}
